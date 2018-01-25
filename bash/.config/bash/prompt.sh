7ccd04f29b59262c6029be402ea62ebbba9533b4
relnod <mail@paul-schiffers.de>
2018-01-25 07:56:41 +0100

    update

--- bash/.config/bash/prompt.sh
+++ bash/.config/bash/prompt.sh
@@ -8,6 +8,6 @@ ARROW_R_BOLD=''
 ARROW_R_THIN=''
 
 ps1="${COLOR1}\u ${END_COLOR}${COLOR2}${ARROW_R_BOLD}${END_COLOR}"
-ps1="$ps1 \W ${COLOR1}$ARROW_R_BOLD "
+ps1="$ps1 \W ${COLOR1}$ARROW_R_BOLD"
 ps1="$ps1 \$(__git_ps1 '%s')${END_COLOR}${COLOR2}$ARROW_R_BOLD${END_COLOR} "
 export PS1=$ps1
