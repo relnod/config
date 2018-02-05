cde9bab49e450177049ba334b18dbcf7aa5c9c7a
relnod <mail@paul-schiffers.de>
2018-02-05 10:57:10 +0100

    update

--- tmux/.config/tmux/utils/statusbar.sh
+++ tmux/.config/tmux/utils/statusbar.sh
@@ -9,42 +9,43 @@ seg_uptime() {
 }
 
 seg_load() {
-    jptime | sed -E 's/.*average: *//; s/,/ |/g'
+    uptime | sed -E 's/.*average: *//; s/,/ |/g'
 }
 
-ARROW_L_BOLD=''
-ARROW_L_THIN=''
-ARROW_R_BOLD=''
-ARROW_R_THIN=''
+arrow_l_bold=''
+arrow_l_thin=''
+arrow_r_bold=''
+arrow_r_thin=''
 
 color_bg_main="black"
-color_bg_inactive="blue"
+color_bg_inactive="colour9"
+
 if [[ $(tmux show-environment TMUX_OFF) == "TMUX_OFF=true" ]]; then
     color_bg_main="$color_bg_inactive"
 fi
 
 if [[ $1 == "window" ]]; then
-    window="#[fg=white,bg=$color_bg_main] #I $ARROW_R_THIN #W "
+    window="#[fg=white,bg=$color_bg_main] #I $arrow_r_thin #W "
     echo $window
 fi
 
 if [[ $1 == "window-current" ]]; then
-    current="#[fg=$color_bg_main,bg=yellow]$ARROW_R_BOLD "
-    current="$current#[fg=black,bg=yellow]#I $ARROW_R_THIN #W "
-    current="$current#[fg=yellow,bg=$color_bg_main]$ARROW_R_BOLD"
+    current="#[fg=$color_bg_main,bg=yellow]$arrow_r_bold "
+    current="$current#[fg=black,bg=yellow]#I $arrow_r_thin #W "
+    current="$current#[fg=yellow,bg=$color_bg_main]$arrow_r_bold"
     echo $current
 fi
 
 if [[ $1 == "left" ]]; then
     left="#[fg=black,bg=white] $(seg_hostname) "
-    left="$left#[fg=white,bg=$color_bg_main]$ARROW_R_BOLD "
+    left="$left#[fg=white,bg=$color_bg_main]$arrow_r_bold "
     echo $left
 fi
 
 if [[ $1 == "right" ]]; then
-    right="#[fg=white,bg=$color_bg_main]$ARROW_L_BOLD"
+    right="#[fg=white,bg=$color_bg_main]$arrow_l_bold"
     right="$right#[fg=black,bg=white] $(seg_uptime)"
-    right="$right#[fg=black,bg=white] $ARROW_L_BOLD"
+    right="$right#[fg=$color_bg_main,bg=white] $arrow_l_bold"
     right="$right#[fg=white,bg=$color_bg_main] $(seg_load)  "
     echo $right
 fi
